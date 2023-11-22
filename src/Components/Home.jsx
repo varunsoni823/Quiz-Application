abe5747f977988c65d6f1e3ebbad25fbc3b5b9fa
Varun Soni <141726392+varunsoni823@users.noreply.github.com>
2023-11-22 21:25:13 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./Home.css"
 const Home = () => {
     return (
-        <div>
-            <h3>Homepage</h3>
-            <Link to="/config">Start Quiz</Link>
+        <>
+        <div className="header">
+            <div className="text">
+            
+                <h6 className="texth6"> Train Your Brain</h6>
+                <h1 className="texth1">Qui<span>zz</span>ee Qui<span>z</span> </h1>
+                <h3>Quick quiz knowledge booter for a perfect level up</h3>
+                <div className="line"></div>
+                <h1 className="texth11">welcome!</h1>
+            </div>
+            <Link to="/config" className="button">Go To Quiz</Link>
+
         </div>
+        </>
     );
 };
 
